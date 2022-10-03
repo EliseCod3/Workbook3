5eabc7e94638eb9a1b611207730d04c6d51481e4
elise bell <eliseraynahasawsomesauce@gmail.com>
2022-10-03 10:58:14 -0400

    Finished exercise

---
+++ Exercises/Functions/simple_functions.js
@@ -0,0 +1,22 @@
+function favoriteThings() {
+    const nam = `My name is Ray`;
+    const movie = 'My favorite movie is all the Transformers movie series';
+    const musician = `My favorite musician is Ariana Grande.`;
+        console.log(nam);
+        console.log(movie);
+        console.log(musician);
+}
+
+function whyImHere() {
+    const reason = `I have joined the learn to code program because I want to be able to live on my own with a cat.`;
+    console.log(reason);
+}
+
+function favoritePlace() {
+    const place = `My favorite place is anywhere quite. I am constantly around chaos so anywhere with peace and quite is my favorite place.`;
+    console.log(place);
+}
+
+favoriteThings();
+whyImHere();
+favoritePlace();
