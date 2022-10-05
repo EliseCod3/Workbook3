c068b638d9260014113eb2b5579eb0c40048a874
elise bell <eliseraynahasawsomesauce@gmail.com>
2022-10-05 09:35:43 -0400

    finished date to strings and setup next exercise

---
+++ Exercises/Dates/DateScripts/date_to_string.js
@@ -0,0 +1,11 @@
+"use strict";
+
+let date = new Date();
+
+console.log(date.toString());
+
+console.log(date.toDateString());
+
+console.log(date.toUTCString());
+
+console.log(date.toLocaleDateString());
