b59871be43332ef9ef4facc1251a0c3267ed96d5
elise bell <eliseraynahasawsomesauce@gmail.com>
2022-10-05 10:47:38 -0400

    finished exercise

---
+++ Exercises/Dates/DateScripts/date_difference.js
@@ -0,0 +1,13 @@
+"use strict";
+
+let startDate = new Date("July 11, 2022");
+let endDate = new Date("November 11, 2022");
+
+let msecPerDay = 1000 * 60 * 60 * 24;
+
+let msecElapsed = endDate.getTime() - startDate.getTime();
+
+let dayDiff = msecElapsed / msecPerDay;
+let numDays = Math.round(dayDiff);
+
+console.log(`The number of days between dates is ${numDays}`);
